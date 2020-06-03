2453def55b324b0ac6398897450ebb3ad5a4f05b
TiagoFernandes2 <tiagofernandes4@gmail.com>
2020-06-03 20:52:04 +0100

    IMappers

---
+++ TP2/ADO.NET/TP2_ADO.NET/Mappers_Interface/IMapperUC_Semestre.cs
@@ -0,0 +1,14 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mappers_Interface
+{
+    public interface IMapperUC_Semestre : IMapper<UC_Semestre, int>
+    {
+
+    }
+}
